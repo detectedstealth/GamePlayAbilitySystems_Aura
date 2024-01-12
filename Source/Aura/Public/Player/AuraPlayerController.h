79fc111cea4b50bd866340c7311138584068fc62
Bruce Wade <bruce.wade@gmail.com>
2024-01-12 08:20:08 -0800

    Added EmemyInterface current used to enable highlighting enemies while hovering over them. Created a enemy base blueprint for inheriting common logic/settings in all children.

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class IEnemyInterface;
 struct FInputActionValue;
 class UInputMappingContext;
 class UInputAction;
@@ -20,6 +21,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
@@ -34,4 +36,8 @@ private:
 
 
 	void Move(const FInputActionValue& InputActionValue);
+	void CursorTrace();
+
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
 };
